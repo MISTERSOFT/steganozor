d2000165d4299feef474170fff2f0c5e42c062b6
Sofiane Hamadi <sofiane.hamadi.pro@gmail.com>
2017-05-18 20:40:23 +0200

    Exception class for height src img > host height

---
+++ steganography/Exceptions/SourceImageHeightGreaterThanHost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steganography.Exceptions
+{
+    public class SourceImageHeightGreaterThanHost : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Source image height is greater than host image height.";
+            }
+        }
+    }
+}
