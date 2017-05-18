4ff66842f0001fd02dd9d4fe0cb93521ddd884f9
Sofiane Hamadi <sofiane.hamadi.pro@gmail.com>
2017-05-18 21:40:36 +0200

    class for messagebox

---
+++ steganography/Alerter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace steganography
+{
+    public class Alerter
+    {
+        public static void Error(string text, string caption)
+        {
+            MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void Info(string text, string caption)
+        {
+            MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
