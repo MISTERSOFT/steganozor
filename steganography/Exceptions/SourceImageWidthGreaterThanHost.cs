097dc50996d92540f35796ed820abffc62887adf
Sofiane Hamadi <sofiane.hamadi.pro@gmail.com>
2017-05-18 20:38:50 +0200

    Exception class for width src img > host img

---
+++ steganography/Exceptions/SourceImageWidthGreaterThanHost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace steganography.Exceptions
+{
+    public class SourceImageWidthGreaterThanHost : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Source image width is greater than host image width.";
+            }
+        }
+    }
+}
